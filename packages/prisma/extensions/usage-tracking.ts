81b3f592b94393262d4171b34033e9fc6c29b549
Ankur Datta <datta@prisma.io>
2024-08-01 16:07:15 +0545

    fix: prisma client not found error

--- packages/prisma/extensions/usage-tracking.ts
+++ packages/prisma/extensions/usage-tracking.ts
@@ -1,4 +1,4 @@
-import { Prisma } from "@prisma/client/default";
+import { Prisma } from "@prisma/client";
 
 import LicenseKeyService, { UsageEvent } from "@calcom/ee/common/server/LicenseKeyService";
 
